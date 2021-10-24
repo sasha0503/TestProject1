cecc130d8f4deaa8c987916b0d9063cbdb7fa35b
Sasha Lyuklyan <lyuklyanoleksandr@gmail.com>
2021-10-24 08:39:12 +0300

    Add project files.

---
+++ ExcelTest.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LabCalculator;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Forms;
+
+namespace TestProject1
+{
+    public class ExcelTest
+    {
+        [TestMethod()]
+        public void Form1Test()
+        {
+            Form1 form = new Form1();
+
+            // Check column naming
+            Assert.AreEqual(form.getDataGridView().Columns[0].Name, "A");
+            Assert.AreEqual(form.getDataGridView().Columns[3].Name, "D");
+            Assert.AreEqual(form.getDataGridView().Columns[25].Name, "Z");
+            Assert.AreEqual(form.getDataGridView().Columns[26].Name, "AA");
+            // Check row naming
+            Assert.AreEqual(form.getDataGridView().Rows[0].HeaderCell.Value, "0");
+            Assert.AreEqual(form.getDataGridView().Rows[4].HeaderCell.Value, "4");
+        }
+
+        [TestMethod()]
+        public void getDataGridViewTest()
+        {
+            Form1 form = new Form1();
+            Assert.IsInstanceOfType(form.getDataGridView(), typeof(DataGridView), "wrong type!");
+        }
+    }
+}
